8ec481673889ad2e9e14df5de0c5a4077d3d2039
bru1f0rc3 <wint1841@inbox.ru>
2024-06-20 21:17:36 +0300

    .

--- EventApplication/Windows/EditActionWindow.xaml.cs
+++ EventApplication/Windows/EditActionWindow.xaml.cs
@@ -48,35 +48,26 @@ namespace EventApplication.Windows
         public StringBuilder FieldCh()
         {
             StringBuilder error = new StringBuilder();
-            ActionEvent _action = (ActionEvent)DataContext;
+            ActionEvent action = (ActionEvent)DataContext;
 
-            if (_action != null)
-            {
-                if (_action.EventDate < new DateTime(2020, 1, 1))
-                    error.AppendLine("Нельзя указывать дату ниже 2020 года.");
-                else if (_action.EventDate > new DateTime(3000, 12, 31))
-                    error.AppendLine("Нельзя указывать дату выше 3000 года.");
-                if (ContainsAnySpecialCharacters(_action.EventTitle))
-                    error.AppendLine("Недопустимые символы в поле Название мероприятия.");
-                if (ContainsAnySpecialCharacters(_action.EventDescription))
-                    error.AppendLine("Недопустимые символы в поле Описание мероприятия.");
-                if (_action.EventTime.HasValue && (_action.EventTime.Value < new TimeSpan(6, 0, 0) || _action.EventTime.Value > new TimeSpan(23, 59, 59)))
-                    error.AppendLine("Время события должно быть не раньше 6:00 и не позже 23:59.");
-            }
-            else
-            {
-                error.AppendLine("Объект события не найден.");
-            }
+            if (_action.EventDate < new DateTime(2020, 1, 1))
+                error.AppendLine("Нельзя указывать дату ниже 2020 года.");
+            else if (_action.EventDate > new DateTime(3000, 12, 31))
+                error.AppendLine("Нельзя указывать дату выше 3000 года.");
+            if (ContainsAnySpecialCharacters(_action.EventTitle))
+                error.AppendLine("Недопустимые символы в поле Название мероприятия.");
+            if (ContainsAnySpecialCharacters(_action.EventDescription))
+                error.AppendLine("Недопустимые символы в поле Описание мероприятия.");
+            if (_action.EventTime.HasValue && (_action.EventTime.Value < new TimeSpan(6, 0, 0) || _action.EventTime.Value > new TimeSpan(23, 59, 59)))
+                error.AppendLine("Время события должно быть не раньше 6:00 и не позже 23:59.");
 
             return error;
         }
 
         public bool ContainsAnySpecialCharacters(string input)
         {
-            if (string.IsNullOrEmpty(input)) return false;
-
             string specialCharacters = @"~`!@#$%^&*_=+[{]}\|;:<>/?";
-            return input.Any(ch => specialCharacters.Contains(ch));
+            return input.IndexOfAny(specialCharacters.ToCharArray()) != -1 && !input.All(char.IsDigit);
         }
     }
 }
