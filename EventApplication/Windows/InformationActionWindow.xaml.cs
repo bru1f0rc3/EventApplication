3ced91735f788ff43642ffcfd66f6b55584f053a
Надир <144530385+bru1f0rc3@users.noreply.github.com>
2025-10-04 23:57:52 +0300

    Refactor InformationActionWindow: improve style and readability

--- EventApplication/Windows/InformationActionWindow.xaml.cs
+++ EventApplication/Windows/InformationActionWindow.xaml.cs
@@ -1,24 +1,38 @@
-﻿using EventApplication.Entities;
+using EventApplication.Entities;
 using System.Windows;
 
 namespace EventApplication.Windows
 {
     /// <summary>
-    /// Логика взаимодействия для InformationActionWindow.xaml
+    /// Окно для отображения информации о действии/событии.
     /// </summary>
     public partial class InformationActionWindow : Window
     {
-        private ActionEvent _action;
-        public InformationActionWindow(ActionEvent selected)
+        private readonly ActionEvent _action;
+
+        /// <summary>
+        /// Инициализирует новый экземпляр окна информации о действии.
+        /// </summary>
+        /// <param name="selectedAction">Выбранное действие для отображения.</param>
+        public InformationActionWindow(ActionEvent selectedAction)
         {
             InitializeComponent();
-            _action = selected;
+            _action = selectedAction;
             DataContext = _action;
         }
 
+        /// <summary>
+        /// Обрабатывает событие нажатия на кнопку подтверждения прочтения информации.
+        /// </summary>
+        /// <param name="sender">Источник события.</param>
+        /// <param name="e">Аргументы события.</param>
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
-            var result = MessageBox.Show("Вы до конца прочитали всю информацию?", "Предупреждение", MessageBoxButton.YesNo);
+            MessageBoxResult result = MessageBox.Show(
+                "Вы до конца прочитали всю информацию?",
+                "Предупреждение",
+                MessageBoxButton.YesNo);
+
             if (result == MessageBoxResult.Yes)
             {
                 Close();
